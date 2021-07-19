a1128e21a1de48721b5ceecabbdfb9573352c9c7
arseniypom <arpomazkov@bk.ru>
2021-07-19 22:35:25 +0300

    Started the project and created server endpoints for user authentication

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const PORT = config.get('port') || 5000
+
+app.use('/api/auth', require('./routes/auth.routes'))
+
+
+async function start() {
+  try {
+    await mongoose.connect(
+      config.get('mongoUri'),
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+      }
+    )
+    app.listen(PORT, () => console.log(`Server launched on port ${PORT}`))
+  } catch (error) {
+    console.log('Server Error:', error.message)
+    process.exit(1)
+  }
+}
+
+start()
---
+++ routes/auth.routes.js
@@ -0,0 +1,95 @@
+const {Router} = require('express')
+const bcrypt = require('bcryptjs')
+const config = require('config')
+const jwt = require('jsonwebtoken')
+const {check, validationResult} = required('express-validator')
+const User = require('../models/User')
+const router = Router()
+
+
+// /api/auth/register
+router.post(
+  '/register',
+  [
+    check('email', 'Invalid Email').isEmail(),
+    check('password', 'Password minimal length is 6 symbols').isLength({min: 6})
+  ],
+  async (req, res) => {
+  try {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+        message: 'Register data is invalid'
+      })
+    }
+
+    const {email, password} = req.body
+
+    const candidate = await User.findOne({email: email})
+    if (candidate) {
+      res.status(400).json({message: 'This email is already registered, please use it to log in'})
+    }
+
+    const hashedPassword = await bcrypt.hash(pasword, 12)
+    const user = User({email, password: hashedPassword})
+
+    await user.save()
+    res.status(201).json({message: 'User has been created'})
+    
+  } catch (error) {
+    res.status(500).json({message: 'Something went wrong, please try again'})
+  }
+})
+
+// /api/auth/login
+router.post(
+  '/login',
+  [
+    check('email', 'Enter correct password').normalizeEmail().isEmail(),
+    check('password', 'Enter correct password').exists()
+  ],
+  async (req, res) => {
+  try {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+        message: 'Login data is invalid'
+      })
+    }
+
+    const {email, password} = req.body
+
+    const user = await User.findOne({email})
+
+    if (!user) {
+      return res.status(400).json({
+        message: `User doesn't exist`
+      })
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: `Password is incorrect, try again`
+      })
+    }
+
+    const token = jwt.sign(
+      {userId: user.id},
+      config.get('jwtSecret'),
+      {expiresIn: '1h'}
+    )
+
+    res.json({token, userId: user.id})
+    
+  } catch (error) {
+    res.status(500).json({message: 'Something went wrong, please try again'})
+  }
+})
+
+module.exports = router
